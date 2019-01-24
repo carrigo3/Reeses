473eb34654ffaf92770af89ddabfad9c8f118abe
Cam Arrigo <carrigo3@gmail.com>
2019-01-23 22:56:47 -0500

    Completed Reese's App

--- Reeses/ViewController.swift
+++ Reeses/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func heyButton(_ sender: UIButton) {
+        let message1 = "You got peanut butter in my chocolate!"
+        let message2 = "You got chocolate in my peanut butter!"
+        
+        if messageLabel.text== message1 {
+            messageLabel.text = message2
+            messageLabel.textColor = UIColor.brown
+            messageLabel.textAlignment = NSTextAlignment.right
+        } else {
+            messageLabel.text = message1
+            messageLabel.textColor = UIColor.orange
+            messageLabel.textAlignment = NSTextAlignment.left
+        }
+    }
+    
 }
 
